7f6e914b9649db237a6de280b7f37cd9bcff0481
Ash Dey <ash@pop-os.localdomain>
2023-11-02 16:56:18 +1100

    communication services

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ data "azurerm_resource_group" "rg" {
   name = "it-${var.env}-${var.region}-${var.company}-${var.proj}-rg"
 }
 
-# Communication service
+# Create Communication Services
 resource "azurerm_communication_service" "acs" {
   name                = "central-communication-service"
   resource_group_name = data.azurerm_resource_group.rg.name
