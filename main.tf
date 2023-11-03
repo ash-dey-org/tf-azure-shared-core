a44493316afd45888fc96e30150cd5cae7f129c5
Ash Dey <ashabc [at] hotmail.com>
2023-11-03 22:19:25 +1100

    communication services

--- main.tf
+++ main.tf
@@ -2,14 +2,14 @@ data "azurerm_resource_group" "rg" {
   name = "${var.env}-${var.region}-${var.company}-rg-${var.proj}"
 }
 
-/*
+
 # Create Communication Services
 resource "azurerm_communication_service" "acs" {
   name                = "central-communication-service"
   resource_group_name = data.azurerm_resource_group.rg.name
   data_location       = var.location
 }
-*/
+
 
 #creat email communnication service
 resource "azurerm_email_communication_service" "aecs" {
